6991f914f5b71515043cb42714d3795c94977841
Pierre Fasce <lemonpiero@gmail.com>
2026-08-20 16:44:55 +0200

    fix routes bug

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
 export { default as AppRouter } from "./AppRouter";
-export { default as ScrollToTop } from "./ScrollToTop";
+export * from "./ScrollToTop";
 export * from "./ProtectedAdminRoute"
 export * from "./ProtectedGuestRoute"
